256e80b771b95f5e5180b6a608c626d2bf520f94
nikolasfil <nikolas13in@gmail.com>
2023-07-26 21:18:53 +0300

    update

--- chat_planner.py
+++ chat_planner.py
@@ -5,9 +5,10 @@ class ExcelSheetApp:
     def __init__(self):
         self.num_rows = 12
         self.num_cols = 8
+        self.size = (15, 2)
         self.layout = [
             [sg.Button("Clear Color", key='-CLEAR-COLOR-'), sg.Button("Clear All",
-                                                                      key='-CLEAR-ALL-'), sg.Text('Navigation'), sg.Button("Check", key='-CHECK-')],
+                                                                      key='-CLEAR-ALL-'), sg.Button("Check", key='-CHECK-')],
         ]
         self.create_input_fields()
 
@@ -17,20 +18,20 @@ class ExcelSheetApp:
                       '15:00-16:00', '16:00-17:00', '17:00-18:00', '18:00-19:00', '19:00-20:00', '20:00-21:00']
 
         # Add days of the week to the first row
-        header_row = [sg.Text('', size=(10, 1))]
+        header_row = [sg.Text('', size=self.size)]
         header_row.extend(
-            [sg.Text(day, size=(10, 1), justification='center') for day in days_of_week])
+            [sg.Text(day, size=self.size, justification='center') for day in days_of_week])
         self.layout.append(header_row)
 
         for row in range(self.num_rows):
             row_layout = []
             # Add time slots to the first column
             row_layout.append(
-                sg.Text(time_slots[row], size=(10, 1), justification='center'))
+                sg.Text(time_slots[row], size=self.size, justification='center'))
 
             for col in range(1, self.num_cols):
                 row_layout.append(
-                    sg.Input(size=(10, 1), key=(row, col), enable_events=True))
+                    sg.Input(size=self.size, key=(row, col), enable_events=True))
             self.layout.append(row_layout)
 
     def run(self):
