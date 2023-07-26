4a58b72f968603da009a6cc5d5c17f0191fd31a6
nikolasfil <nikolas13in@gmail.com>
2023-07-26 21:14:21 +0300

    update

--- chat_planner.py
+++ chat_planner.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 class ExcelSheetApp:
     def __init__(self):
         self.num_rows = 12
-        self.num_cols = 7
+        self.num_cols = 8
         self.layout = [
             [sg.Button("Clear", key='-CLEAR-'), sg.Text('Navigation'),
              sg.Button("Check", key='-CHECK-')],
@@ -12,9 +12,23 @@ class ExcelSheetApp:
         self.create_input_fields()
 
     def create_input_fields(self):
+        days_of_week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        time_slots = ['09:00-10:00', '10:00-11:00', '11:00-12:00', '12:00-13:00', '13:00-14:00', '14:00-15:00',
+                      '15:00-16:00', '16:00-17:00', '17:00-18:00', '18:00-19:00', '19:00-20:00', '20:00-21:00']
+
+        # Add days of the week to the first row
+        header_row = [sg.Text('', size=(10, 1))]
+        header_row.extend(
+            [sg.Text(day, size=(10, 1), justification='center') for day in days_of_week])
+        self.layout.append(header_row)
+
         for row in range(self.num_rows):
             row_layout = []
-            for col in range(self.num_cols):
+            # Add time slots to the first column
+            row_layout.append(
+                sg.Text(time_slots[row], size=(10, 1), justification='center'))
+
+            for col in range(1, self.num_cols):
                 row_layout.append(
                     sg.Input(size=(10, 1), key=(row, col), enable_events=True))
             self.layout.append(row_layout)
@@ -35,12 +49,13 @@ class ExcelSheetApp:
 
     def clear_input_fields(self):
         for row in range(self.num_rows):
-            for col in range(self.num_cols):
+            for col in range(1, self.num_cols):
                 self.window[(row, col)].update('')
+                self.window[(row, col)].update(background_color='white')
 
     def check_and_color_inputs(self):
-        for row in range(self.num_rows):
-            for col in range(self.num_cols):
+        for row in range(1, self.num_rows):  # Exclude the header row
+            for col in range(1, self.num_cols):
                 value = self.window[(row, col)].get()
                 if value == '1':
                     self.window[(row, col)].update(background_color='blue')
