1319b00da23e09345e86b81625f6dd2d02159067
nikolasfil <nikolas13in@gmail.com>
2023-07-26 21:01:27 +0300

    update

--- chat_planner.py
+++ chat_planner.py
@@ -1,36 +1,56 @@
 import PySimpleGUI as sg
 
-# Define the layout for the window
-layout = [
-    [sg.Button("Previous", key='-PREV-'), sg.Text('Navigation'),
-     sg.Button("Next", key='-NEXT-')],
-]
 
-# Add 12 input fields and arrange them in 7 columns
-num_rows = 12
-num_cols = 7
-for row in range(num_rows):
-    row_layout = []
-    for col in range(num_cols):
-        row_layout.append(sg.Input(size=(10, 1), key=(row, col)))
-    layout.append(row_layout)
+class ExcelSheetApp:
+    def __init__(self):
+        self.num_rows = 12
+        self.num_cols = 7
+        self.layout = [
+            [sg.Button("Clear", key='-CLEAR-'),
+             sg.Button("Check", key='-CHECK-')],
+        ]
+        self.create_input_fields()
 
-# Create the window
-window = sg.Window("Excel Sheet", layout, finalize=True)
+    def create_input_fields(self):
+        for row in range(self.num_rows):
+            row_layout = []
+            for col in range(self.num_cols):
+                row_layout.append(
+                    sg.Input(size=(10, 1), key=(row, col), enable_events=True))
+            self.layout.append(row_layout)
 
-# Event loop
-while True:
-    event, values = window.read()
+    def run(self):
+        self.window = sg.Window("Excel Sheet", self.layout, finalize=True)
+        while True:
+            event, values = self.window.read()
 
-    if event == sg.WIN_CLOSED:
-        break
-    elif event == '-PREV-':
-        # Handle the previous button click
-        # You can implement the logic for navigation here
-        pass
-    elif event == '-NEXT-':
-        # Handle the next button click
-        # You can implement the logic for navigation here
-        pass
+            if event == sg.WIN_CLOSED:
+                break
+            elif event == '-CLEAR-':
+                # Handle the previous button click
+                # You can implement the logic for navigation here
+                pass
+            elif event == '-CHECK-':
+                # Handle the next button click
+                # You can implement the logic for navigation here
+                print('Check')
+            else:
+                self.check_and_color_input(event)
 
-window.close()
+        self.window.close()
+
+    def check_and_color_input(self, event):
+        try:
+            row, col = event
+            value = self.window[event].get()
+            if value == '1':
+                self.window[event].update(background_color='blue')
+            else:
+                self.window[event].update(background_color='white')
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    app = ExcelSheetApp()
+    app.run()
